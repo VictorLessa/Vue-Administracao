e8a56ea67c45be9b51825c1217a27d6ed798d256
VictorLessa <victordsgnr@gmail.com>
2018-10-12 22:55:40 -0300

    axios

---
+++ src/features/auth/store/webapi.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const login = async (credential) => {
+  let config = {
+    method: 'POST',
+    url: 'http://207.246.115.47:8081/api/1.0.0/pharmacy/login',
+    data: {
+      user: credential.user,
+      password: credential.password
+    }
+  }
+  return axios.request(config)
+}
+
+export default {
+  login
+}
